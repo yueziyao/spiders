c70b030e74e636a60efaa6a507e84b569666f750
iced-log <yueziyao9@163.com>
2020-10-10 00:09:26 +0800

    add

--- weather/getWeather.py
+++ weather/getWeather.py
@@ -3,8 +3,7 @@ import json
 
 def getone():
         r = requests.get('https://devapi.heweather.net/v7/weather/3d?location=101040700&key=7e4459e12a764f1fa4bc134b2073d5bc')
-        print(r)
-        dic = json.loads(r)
+        dic = json.loads(r.text).get('daily')
         print(dic)
 
 getone()
