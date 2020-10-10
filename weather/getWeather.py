78fdb849b66bdf3d9cee1e5253bbbeb0582d8256
岳子尧 <yueziyao@travelsky.com>
2020-10-10 11:13:53 +0800

    123

--- weather/getWeather.py
+++ weather/getWeather.py
@@ -3,10 +3,12 @@ import json
 import pandas as pd
 import time
 
+import os
 import db
 
 def getweather():
-        r = requests.get('https://devapi.heweather.net/v7/weather/3d?location=101040700&key=7e4459e12a764f1fa4bc134b2073d5bc')
+        apikey = os.getenv('APIKEY')
+        r = requests.get('https://devapi.heweather.net/v7/weather/3d?location=101040700&key='+apikey)
         dic = json.loads(r.text).get('daily')
         for d in dic:
                 d['insertTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
