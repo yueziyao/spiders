f60a5e09c166b616ad0ba38f5ef322a9f5eb8554
iced-log <yueziyao9@163.com>
2020-10-11 19:48:02 +0800

    edit

--- weather/getWeather.py
+++ weather/getWeather.py
@@ -11,9 +11,9 @@ apikey = os.getenv('APIKEY')
 def getweather():
         r = requests.get('https://devapi.heweather.net/v7/weather/3d?location=101040700&key='+apikey)
         dic = json.loads(r.text).get('daily')
-        for d in dic:
-                d['insertTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        data= pd.DataFrame(dic)
+        todayWeather = d[0]
+        todayWeather['insertTime'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        data = pd.DataFrame(todayWeather)
         db.insertWeather(data)
 
 getweather()
