7703e3b938ced0b0401e50eaedae2a761d5fff87
iced-log <yueziyao9@163.com>
2020-10-11 20:35:52 +0800

    edit

--- weather/db.py
+++ weather/db.py
@@ -16,19 +16,10 @@ def insertWeather(data):
 
     db = cilent["app"] #连接mydb数据库，没有则自动创建
     collection = db["daily"]
-    str = json.loads(data.T.to_json()).values()
-
-    res = collection.insert_many(str)
-    
-
-def cleanData():
-    cilent = pymongo.MongoClient(url,
-        username=username,
-        password=password,
-        authSource='admin',
-        authMechanism='SCRAM-SHA-1')
-
-    db = cilent["app"] #连接mydb数据库，没有则自动创建
-    collection = db["daily"]
+    # 清空
     collection.remove()
-    print('clean over')
+    print('清空成功')
+    # 入库
+    str = json.loads(data.T.to_json()).values()
+    collection.insert_many(str)
+    print('入库成功')
