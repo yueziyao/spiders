56829214536893fd1d03f4797bdd1a8642070199
iced-log <yueziyao9@163.com>
2020-10-11 19:42:06 +0800

    add

--- weather/db.py
+++ weather/db.py
@@ -29,10 +29,5 @@ def cleanData():
 
     db = cilent["app"] #连接mydb数据库，没有则自动创建
     collection = db["daily"]
-    for id in collection.distinct('fxDate'):#使用distinct方法，获取每一个独特的元素列表
-        num = collection.count({"fxDate":id})#统计每一个元素的数量
-        if(num > 1):
-            a = collection.find({"fxDate":id}).sort('time',pymongo.ASCENDING)
-            lists = list(a)
-            collection.remove({"fxDate":id ,'insertTime':lists[0]['insertTime']},0)
-            print('重复issue数据 id = %s ,删除入库时间： %s,保留入库时间：%s' %(id,lists[0]['insertTime'],lists[1]['insertTime']))
+    collection.remove()
+    print('clean over')
